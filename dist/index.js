c15ec2302c9250823749bd14e8075f1d80c1f7b8
WRHR <wrhreeves@gmail.com>
2021-01-15 21:39:50 -0700

    Setup Redis and cookies, add user mutaitons

--- dist/index.js
+++ dist/index.js
@@ -14,14 +14,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const dotenv_1 = __importDefault(require("dotenv"));
+const constants_1 = require("./constants");
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const type_graphql_1 = require("type-graphql");
 const typeorm_1 = require("typeorm");
+const ioredis_1 = __importDefault(require("ioredis"));
+const express_session_1 = __importDefault(require("express-session"));
+const connect_redis_1 = __importDefault(require("connect-redis"));
 const hello_1 = require("./resolvers/hello");
 const Habit_1 = require("./entities/Habit");
 const habit_1 = require("./resolvers/habit");
 const User_1 = require("./entities/User");
+const user_1 = require("./resolvers/user");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     dotenv_1.default.config();
     yield typeorm_1.createConnection({
@@ -32,12 +37,30 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         entities: [Habit_1.Habit, User_1.User],
     });
     const app = express_1.default();
+    const RedisStore = connect_redis_1.default(express_session_1.default);
+    const redis = new ioredis_1.default();
+    app.use(express_session_1.default({
+        name: constants_1.COOKIE_NAME,
+        store: new RedisStore({
+            client: redis,
+            disableTouch: true,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+            httpOnly: true,
+            sameSite: "lax",
+            secure: constants_1.__prod__,
+        },
+        saveUninitialized: false,
+        secret: `${process.env.REDIS_SECRET}`,
+        resave: false,
+    }));
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: yield type_graphql_1.buildSchema({
-            resolvers: [hello_1.HelloResolver, habit_1.HabitResolver],
+            resolvers: [hello_1.HelloResolver, habit_1.HabitResolver, user_1.UserResolver],
             validate: false,
         }),
-        context: ({ req, res }) => ({ req, res }),
+        context: ({ req, res }) => ({ req, res, redis }),
     });
     apolloServer.applyMiddleware({
         app,
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+
 import { MyContext } from "./types";
 
 import { HelloResolver } from "./resolvers/hello";
 import { Habit } from "./entities/Habit";
 import { HabitResolver } from "./resolvers/habit";
 import { User } from "./entities/User";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   dotenv.config();
@@ -24,12 +30,34 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: `${process.env.REDIS_SECRET}`,
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, HabitResolver],
+      resolvers: [HelloResolver, HabitResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({ req, res }),
+    context: ({ req, res }): MyContext => ({ req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
