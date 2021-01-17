36522fad5aa6221ed8c37053cfb2f2c6719fbf18
WRHR <wrhreeves@gmail.com>
2021-01-16 23:30:22 -0700

    Add foriegn key into habitstreak entity

--- src/entities/HabitStreak.ts
+++ src/entities/HabitStreak.ts
@@ -1,4 +1,11 @@
-import { CreateDateColumn, Entity, ManyToOne, UpdateDateColumn } from "typeorm";
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { Field, ObjectType } from "type-graphql";
 import { Habit } from "./Habit";
 import { Streak } from "./Streak";
@@ -6,10 +13,18 @@ import { Streak } from "./Streak";
 @ObjectType()
 @Entity()
 export class HabitStreak {
+  @PrimaryColumn()
+  habitId: number;
+
+  @PrimaryColumn()
+  streakId: number;
+
   @ManyToOne(() => Habit, (habit) => habit.streakConnection)
+  @JoinColumn({ name: "habitId" })
   habit: Habit;
 
   @ManyToOne(() => Streak, (streak) => streak.habitConnection)
+  @JoinColumn({ name: "streakId" })
   streak: Streak;
 
   @Field(() => String)
